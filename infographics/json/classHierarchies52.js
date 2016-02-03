034e64f4680a87229d20bbf54b59c75e70e8ddd3
Pei Chang <pchang@hawaiirg.com>
2016-02-02 21:58:18 -0800

    other class hierarchies added

---
+++ infographics/json/classHierarchies52.js
@@ -0,0 +1,28 @@
+var parsedJSON = require('./vdm.json');
+var subClass = [], result;
+if (parsedJSON.classes) {
+    var classLength = parsedJSON.classes.length;
+    for (var i = 0; i < classLength; i++) {
+        var cls = parsedJSON.classes[i];
+        if (cls.fmDD === 'fmdd:52') {
+            for (var j = 0; j < cls.properties.length; j++) {
+                var prop = cls.properties[j];
+                if (prop.datatype === '[OBJECT]' && prop.range) {
+                    subClass.push({
+                        "name": prop.range.label + ' (' + prop.fmDD.replace('fmdd:', '') + ')'
+                    });
+                }
+            }
+            subClass.sort(function(a, b) {
+                return a.name.localeCompare(b.name);
+            });
+            result = {
+                name: "prescription (52)",
+                children: subClass
+            };
+
+        }
+    }
+}
+
+console.log(JSON.stringify(result));
